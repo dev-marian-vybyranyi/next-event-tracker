842cd892ea0765d7af58c47b7a031b2f97ad5d3d
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-01-05 14:08:27 +0200

    add event search component and enhance button to support non-link usage

--- components/UI/Button.js
+++ components/UI/Button.js
@@ -1,7 +1,11 @@
 import Link from "next/link";
 import classes from "./Button.module.css";
 
-const Button = ({ children, link }) => {
+const Button = ({ children, link, onClick }) => {
+  if (!link) {
+    return <button className={classes.btn} onClick={onClick}>{children}</button>;
+  }
+
   return (
     <Link href={link} className={classes.btn}>
       {children}
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,10 +1,13 @@
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/EventList";
+import EventsSearch from "../../components/events/EventsSearch";
+
 const AllEventsPage = () => {
   const events = getAllEvents();
 
   return (
     <div>
+      <EventsSearch />
       <EventList items={events} />
     </div>
   );
