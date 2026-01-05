52121ad32fac74c94f073c1cb0741ba0ef65ceaa
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-01-05 14:15:14 +0200

    Implement event search functionality with form submission and dynamic routing.

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,20 @@
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/EventList";
 import EventsSearch from "../../components/events/EventsSearch";
+import { useRouter } from "next/router";
 
 const AllEventsPage = () => {
   const events = getAllEvents();
+  const router = useRouter();
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  }
 
   return (
     <div>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </div>
   );
