ad2375752055c3474ae0d7e8cc300fb22322be6f
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-01-05 15:30:49 +0200

    Configure revalidation and blocking fallback for static pages, update event fetching to featured events, and enhance event detail loading UI.

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,13 +1,17 @@
 import EventContent from "../../components/event-detail/event-content";
 import EventLogistics from "../../components/event-detail/event-logistics";
 import EventSummary from "../../components/event-detail/event-summary";
-import { getEventById, getEvents } from "../../helpers/api-util";
+import { getEventById, getFeaturedEvents } from "../../helpers/api-util";
 
 const EventDetailPage = (props) => {
   const event = props.selectedEvent;
 
   if (!event) {
-    return <p>No event found!</p>;
+    return (
+      <div className="center">
+        <p>Loading...</p>
+      </div>
+    );
   }
 
   return (
@@ -26,23 +30,24 @@ const EventDetailPage = (props) => {
   );
 };
 
-export async function getStaticProps(context) {
-  const eventId = context.params.eventId;
+export async function getStaticProps(conext) {
+  const eventId = conext.params.eventId;
   const event = await getEventById(eventId);
 
   return {
     props: {
       selectedEvent: event,
     },
+    revalidate: 30,
   };
 }
 
 export async function getStaticPaths() {
-  const events = await getEvents();
+  const events = await getFeaturedEvents();
 
   return {
     paths: events.map((event) => ({ params: { eventId: event.id } })),
-    fallback: true,
+    fallback: "blocking",
   };
 }
 
